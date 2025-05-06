7f4cc25989989cb5d7d009eb86c765d84b05a0ca
Zelkiron <140980003+Zelkiron@users.noreply.github.com>
2025-05-06 02:31:56 -0400

    added basic components

---
+++ src/lib/components/NavBar/NavBar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { Component } from "svelte";
+    type Link = {
+        href: string;
+        value: string;
+    }
+    const links: Link[] = [
+        {
+            href: '/',
+            value: 'home'
+        },
+        {
+            href: '/about',
+            value: 'about me'
+        },
+        {
+            href: '/projects',
+            value: 'my projects'
+        },
+        {
+            href: '/resume',
+            value: 'resume'
+        }
+    ];
+</script>
+<div class="flex justify-center items-center mt-2">
+    {#each links as link}
+        <a class="bg-blue-500 font-bold text-white m-1 py-2 px-4 hover:bg-blue-700 rounded-2xl" href='{link.href}'>{link.value}</a>
+    {/each}
+</div>
---
+++ src/lib/templates/ComingSoon/ComingSoon.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import { NavBar } from "$lib/components/NavBar";
+</script>
+<NavBar />
+<h1 class='text-center font-bold text-3xl'>More coming soon...</h1>
